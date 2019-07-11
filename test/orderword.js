30f553d9a6b3311317977666809730ece0900dc1
crisanlucid <crisan.lucid@yahoo.com>
2019-07-11 10:23:37 +0200

    - update test order words

---
+++ test/orderword.js
@@ -0,0 +1,3 @@
+const order = text => {};
+
+module.exports = order;
---
+++ test/orderwords.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const order = require('./orderword.js/index.js.js');
+
+// "e2st 4test Ce1ci un3"                                 => "Ceci est un test"
+// "m6orning 1I do3jo tha5n m4ore l2ove"                  => "I love dojo more than morning"
+// "3au 2mei s5a 8echipa l6ucreze Stu1dentii 7in inva4tat" => "Studentii mei au invatat sa lucreze in echipa"
+
+describe('Return the ordered phrase', () => {
+  it('should be loaded', () => {
+    assert.equal(true, true);
+  });
+});
